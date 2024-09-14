c022a4117413ff7259b6a87cb58e94d11649be25
Marc-hei <157007960+Marc-hei@users.noreply.github.com>
2024-09-14 15:13:37 +0200

    removed index.js again

--- index.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.getElementById('menu-toggle');
-  hamburger.addEventListener('click', () => {
-      hamburger.classList.toggle('cross');
-  });
-
-function toggleDropdown() {
-    let dropdownContent = document.querySelector('.dropdown-content');
-    dropdownContent.classList.toggle('show');
-}
